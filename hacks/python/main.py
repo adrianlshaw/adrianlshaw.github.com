1c7ce2136ca51ad165c7263c31a5f1be7a6ded56
Adrian L. Shaw <adrianlshaw@gmail.com>
2024-03-31 21:17:33 +0100

    Remove graph when input is empty

--- hacks/python/main.py
+++ hacks/python/main.py
@@ -54,4 +54,6 @@ async def show_graph(input_string):
 
     fig.set_size_inches(4, 3)
 
-    display(fig, target="mpl")
+    if inputstring != "":
+        display(fig, target="mpl")
+    plt.close(fig)
